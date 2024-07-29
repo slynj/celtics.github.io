6ae1998730743c4fa5045cee594ef7b4ed2a9eb9
Lyn <lisa.s.jeong@gmail.com>
2024-07-29 10:05:49 -0400

    Updated sharing function title and description

--- js/share.js
+++ js/share.js
@@ -1,8 +1,24 @@
+const url = "slynj.github.io/celtics";
 
-function shareLink() {
-    const message = "Yo Check This Out (ft. Zach Cramer):  ";
-    const url = "https://lynjeong.com";
-    const fullMessage = message + url;
+// function shareLink() {
+//     const message = "Yo Check This Out (ft. Zach Cramer) I'm  ";
+//     const url = "https://lynjeong.com";
+//     const fullMessage = message + url;
+//     const encodedMessage = encodeURIComponent(fullMessage);
+//     const smsUrl = 'sms:?body=' + encodedMessage;
+
+//     window.location.href = smsUrl;
+// }
+
+function setShare() {
+    var resultImg = document.querySelector('#resultImg');
+    var resultAlt = resultImg.firstElementChild.alt;
+    const shareURL = url + "pages/result-" + resultAlt + ".html";
+    const shareDes = infoList[resultAlt].name;
+
+    const message = "Yo Check This Out (ft. Zach Cramer) I'm " + shareDes + "\n\n";
+    // const url = "https://lynjeong.com";
+    const fullMessage = message + shareURL;
     const encodedMessage = encodeURIComponent(fullMessage);
     const smsUrl = 'sms:?body=' + encodedMessage;
 
